a52276392ff2e0838157f84acebb3d06a4d8c150
kira-quan <kira.a.quan@gmail.com>
2014-03-17 10:35:43 -0600

    Created a basic CassandraClient

--- src/jvm/twitter/beer/CassandraClient.java
+++ src/jvm/twitter/beer/CassandraClient.java
@@ -7,10 +7,12 @@ import com.datastax.driver.core.Metadata;
 public class CassandraClient{
 
 	private Cluster cluster;
+	private Session session;
 
 	public void connect(String node){
 		cluster = Cluster.builder().addContactPoint(node).build();
 		Metadata metadata = cluster.getMetadata();
+		session = cluster.connect();
 
 		// TODO: Remove this after testing
 		System.out.printf("Connected to cluster: %s\n", metadata.getClusterName());
@@ -22,5 +24,23 @@ public class CassandraClient{
 	public void close(){
 		// Shutdown cluster instance when finished
 		cluster.shutdown();
+	}
+
+	public void createSchema(String keyspace, String schemaClass){
+		session.execute("CREATE KEYSPACE " + keyspace + " WITH replication= {'class': '" + schemaClass + "', 'replication_factor': 3};");
+	}
+
+	public void createTable(String keyspace, String tableName, String columns){
+		session.execute("CREATE TABLE " + keyspace + "." + tableName + "(" + columns ");");
+	}
+
+	public void loadData(String insertStatement){
+		// Change this to include Prepared and Bound Statements once the database schema has been implemented
+		// http://www.datastax.com/documentation/developer/java-driver/1.0/java-driver/quick_start/qsSimpleClientBoundStatements_t.html
+		session.execute(insertStatement);
+	}
+
+	public void querySchema(String selectStatement){
+		session.execute(selectStatement);
 	}
 }
