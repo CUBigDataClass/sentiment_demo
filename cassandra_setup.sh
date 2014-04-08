4e49f4dd71814d73b99fc23b452926a0ae0aaacf
kira-quan <kira.a.quan@gmail.com>
2014-04-07 22:53:49 -0600

    added cassandra set up

---
+++ cassandra_setup.sh
@@ -0,0 +1,11 @@
+CREATE KEYSPACE twitterCREATE KEYSPACE twitter WITH replication = {
+  'class': 'SimpleStrategy',
+  'replication_factor': '1'
+};
+
+USE twitter;
+
+CREATE TABLE tweet (
+  tweet_id int,
+  tweet text,
+  PRIMARY KEY (tweet_id);
