7b0d6996031d980baa7b6906979ff2400e7ab002
kira-quan <kira.a.quan@gmail.com>
2014-04-09 18:28:41 -0600

    storm with kafka

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 sentiment_demo
 ==============
 To Run:
+<br/>
 mvn -f pom.xml clean install  
 <br/>
 storm jar target/twitter-beer-0.0.1-SNAPSHOT.jar twitter.beer.TwitterTopology
--- src/jvm/twitter/beer/TwitterTopology.java
+++ src/jvm/twitter/beer/TwitterTopology.java
@@ -8,14 +8,28 @@ import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.spout.SchemeAsMultiScheme;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
+
 import twitter.beer.spout.TwitterSpout;
 import twitter.beer.bolt.CassandraBolt;
 import twitter.beer.bolt.ExtractBolt;
+
+import storm.kafka.trident.GlobalPartitionInformation;
+import storm.kafka.BrokerHosts;
+import storm.kafka.Broker;
+import storm.kafka.StaticHosts;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.KafkaSpout;
+
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
 
 // A basic topology that reads from the Twitter stream and does basic processing on the data
 public class TwitterTopology {
@@ -24,7 +38,18 @@ public class TwitterTopology {
 		// Create a new Topology
 		TopologyBuilder builder = new TopologyBuilder();
 
-		builder.setSpout("tweetSpout", new TwitterSpout(), 10);
+		// Set up KafkaSpout
+		GlobalPartitionInformation hostsAndPartitions = new GlobalPartitionInformation();
+		hostsAndPartitions.addPartition(0, new Broker("localhost", 9092));
+		BrokerHosts brokerHosts = new StaticHosts(hostsAndPartitions);
+
+		SpoutConfig kafkaSpoutConfig = new SpoutConfig(brokerHosts, "tweets", "", "storm");
+		kafkaSpoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+		kafkaSpoutConfig.forceFromStart = true;
+
+	
+		//builder.setSpout("tweetSpout", new TwitterSpout(), 10);
+		builder.setSpout("tweetSpout", new KafkaSpout(kafkaSpoutConfig), 10);
 		builder.setBolt("tweetVal", new ExtractBolt(), 3).shuffleGrouping("tweetSpout");
 		builder.setBolt("cassandra", new CassandraBolt(), 3).shuffleGrouping("tweetVal");
 
